f30d17ba131f7b3125cbbb7856bb858b2ea25626
ariqfkhrii <ariqfkhrii@gmail.com>
2025-05-18 17:26:10 +0700

    feat(Product): add search bar and category dropdown to index interface

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -4,9 +4,39 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" rel="stylesheet">
     @vite('resources/js/app.js')
 </head>
 <body>
+    <!-- Search Bar -->
+    <div class="row m-2">
+        <div class="container py-5">
+            <div class="search-wrapper">
+                <div class="search-box">
+                    <input type="text" class="search-input form-control" placeholder="Search anything...">
+                    <i class="fas fa-search search-icon"></i>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- Category Dropdown -->
+    <div class="d-flex justify-content-center mb-5">
+        <div class="dropdown">
+            <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownCategoryButton" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                Category
+            </button>
+            <div class="dropdown-menu text-center mx-auto" aria-labelledby="dropdownCategoryButton" style="min-width: auto;">
+                <a class="dropdown-item" href="#">Food</a>
+                <a class="dropdown-item" href="#">Beverage</a>
+                <a class="dropdown-item" href="#">Household</a></a>
+                <a class="dropdown-item" href="#">Healthcare</a>
+                <a class="dropdown-item" href="#">Personal Care</a>
+            </div>
+        </div>
+    </div>
+
+    <!-- Catalog Product -->
     <div class="row m-2">
          @foreach ( $product as $p)
         <div class="col-md-4 mt-2 mb-2">
